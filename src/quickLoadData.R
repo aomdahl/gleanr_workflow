36d0a9714e3cb2ae3044d5dcac9f7525b2ce47b6
Ashton Omdahl <aomdahl@gmail.com>
2021-09-07 15:45:36 -0400

    Helpful for debugging

---
+++ src/quickLoadData.R
@@ -0,0 +1,66 @@
+pacman::p_load(data.table, tidyr, dplyr, readr, ggplot2, stringr, penalized, cowplot, parallel, doParallel, flashr)
+source("/Users/ashton/Documents/JHU/Research/LocalData/snp_network/sn-spMF/gwas_spMF/src/fit_F.R")
+source("/Users/ashton/Documents/JHU/Research/LocalData/snp_network/sn-spMF/gwas_spMF/src/update_FL.R")
+source("/Users/ashton/Documents/JHU/Research/LocalData/snp_network/sn-spMF/gwas_spMF/src/fit_L.R")
+source("/Users/ashton/Documents/JHU/Research/LocalData/snp_network/sn-spMF/gwas_spMF/src/plot_functions.R")
+source('/Users/ashton/Documents/JHU/Research/LocalData/snp_network/sn-spMF/gwas_spMF/src/compute_obj.R')
+quickLoadFactorization <- function(error_type)
+{
+  z_scores <- fread("~/Documents/JHU/Research/LocalData/snp_network/seed2_thresh0.9_h2-0.1_vars1e-5/seed2_thresh0.9_h2-0.1_vars1e-5.FULL_LIST.1e-5.z.tsv") %>% drop_na() %>% arrange(ids)
+  names <- scan("~/Documents/JHU/Research/LocalData/snp_network/seed2_thresh0.9_h2-0.1_vars1e-5/seed2_thresh0.9_h2-0.1.names.tsv", what = character())
+  z <- as.matrix(z_scores %>% select(-ids))
+  any(duplicated(z_scores$ids))
+  
+  rsid_map <- fread("~/Documents/JHU/Research/LocalData/snp_network/seed2_thresh0.9_h2-0.1_vars1e-5/seed2_thresh0.9_h2-0.1_vars1e-5.pruned_rsids.txt", header =FALSE) %>% rename("ids" = V1, "rsids" = V2)
+  
+  W_se <- fread("~/Documents/JHU/Research/LocalData/snp_network/seed2_thresh0.9_h2-0.1_vars1e-5/seed2_thresh0.9_h2-0.1_vars1e-5.FULL_LIST.1e-5.se.tsv") %>% 
+    drop_na() %>% filter(ids %in% z_scores$ids) %>% arrange(ids) %>% select(-ids) %>% as.matrix()
+  W_se <- 1/ W_se
+  
+  W_maf <- fread("~/Documents/JHU/Research/LocalData/snp_network/seed2_thresh0.9_h2-0.1_vars1e-5/seed2_thresh0.9_h2-0.1_vars1e-5.FULL_LIST.1e-5.maf.tsv") %>% drop_na() %>% filter(ids %in% z_scores$ids) %>% arrange(ids) %>% select(-ids) %>% as.matrix()
+  
+  W_varmaf <- 1/matrix(apply(W_maf, 2, function(x) 2*x*(1-x)), nrow = nrow(W_maf), ncol = ncol(W_maf))
+  
+  betas <- fread("~/Documents/JHU/Research/LocalData/snp_network/seed2_thresh0.9_h2-0.1_vars1e-5/seed2_thresh0.9_h2-0.1_vars1e-5.FULL_LIST.1e-5.beta.tsv") %>% drop_na() %>% filter(ids %in% z_scores$ids) %>% arrange(ids) %>% select(-ids) %>% as.matrix() 
+  all_ids <- fread("~/Documents/JHU/Research/LocalData/snp_network/seed2_thresh0.9_h2-0.1_vars1e-5/seed2_thresh0.9_h2-0.1_vars1e-5.FULL_LIST.1e-5.beta.tsv") %>% drop_na() %>% filter(ids %in% z_scores$ids) %>% arrange(ids) %>% select(ids)
+  W_ones <- matrix(1,nrow = nrow(W_maf), ncol = ncol(W_maf))
+  n <- scan("/Users/ashton/Documents/JHU/Research/LocalData/snp_network/matrix_sparsification/seed2_thresh0.9_h2-0.1.names.tsv", what = character())
+  #id like to have the sample sizes too....  
+  if(error_type == "B_SE")
+  {
+    ret_dat <- betas
+    ret_err <- W_se
+  } else if(error_type == "B_MAF"){
+    ret_dat <- betas
+    ret_err <- W_varmaf
+    
+  }else{
+    ret_dat <- z
+    ret_err <- matrix(1, nrow(z), ncol(z))
+  }
+  option <- list()
+  option[['K']] <- 15
+  option[['alpha1']] <- 1
+  option[['lambda1']] <- 1
+  option[['iter']] <- 30
+  option[['ones_mixed_std']] <- FALSE
+  option[['ones_mixed_ref']] <- FALSE
+  option[['ones']] <- FALSE
+  option[["fixed_ubiq"]] <- FALSE
+  option[['disp']] <- FALSE
+  option[['convF']] <- 0
+  option[['convO']] <- 0
+  option[['ones_mixed_std']] <- FALSE
+  option[["ones_mixed_ref"]] <- FALSE
+  option[['ones_eigenvect']] <- TRUE
+  option[['ones_plain']] <- FALSE
+  option[['reweighted']] <- FALSE
+  option[["glmnet"]] <- FALSE
+  option[["parallel"]] <- TRUE
+  option[["ridge_L"]] <- FALSE
+  option[["preinitialize"]] <- FALSE
+  option[['fastReg']] <- FALSE
+  option$traitSpecificVar <- TRUE
+  #Store stats here
+  return(list("option" = option, "X" = ret_dat, "W" = ret_err, "names" = n))
+}
