b310214f0699b01b2f1f97ef000c912718558eaa
Ashton Omdahl <aomdahl@gmail.com>
2022-03-14 15:53:41 -0400

    Added visualization functionality

--- src/sparsity.R
+++ src/sparsity.R
@@ -24,17 +24,19 @@ approxAvgSparsity <- function(dat, thres){
   sum(colSums(abs(dat) < thresh))/nrow(dat) /ncol(dat)
 }
 #quick script to choose the traits we want
-pacman::p_load(data.table, tidyr, dplyr, readr, ggplot2, stringr, Xmisc)
+pacman::p_load(data.table, tidyr, dplyr,  ggplot2, stringr, Xmisc)
 parser <- ArgumentParser$new()
 parser$add_description("Script to quickly assess the sparsity of a matrix by column")
 parser$add_argument("--data", type = 'character', help = "Path to data file")
-parser$add_argument('--figure',type='logical',action='store_true', help='Do you want to make a figure with the data', default = FALSE)
+parser$add_argument('-hist',type='character', help='Do you want to make a figure with the data? give the path', default = "")
+parser$add_argument('--figure',type='character', help='Do you want to make a figure with the data? give the path', default = "")
+parser$add_argument('--approxthresh',type='numeric',help='Specify the sparsity threshold', default = 1e-4)
 parser$add_argument('--approx',type='logical',action='store_true', help='Give approximate sparsity', default = FALSE)
 parser$add_argument('--omit_first',type='logical',action='store_true', help='Omit the first column in calculation', default = FALSE)
 parser$add_argument('--help',type='logical',action='store_true',help='Print the help page')
 parser$helpme()
 args <- parser$get_args()
-options(readr.num_columns = 0)
+#options(readr.num_columns = 0)
 
 if(FALSE)
 {
@@ -51,8 +53,9 @@ if(args$omit_first)
 if(args$approx)
 {
   #Approximate sparsity
-  sparsity <- approxSparsity(dat)
+  sparsity <- approxSparsity(dat, thresh = args$approxthresh)
   print("Approximate sparsity:")
+  print(sparsity)
 } else{
   #Exact sparsity 
   sparsity <- exactSparsity(dat)
@@ -60,6 +63,17 @@ if(args$approx)
 }
 
 print(summary(sparsity))
-#plot(sparsity)
+if(args$figure != "")
+{
+    png(args$figure)
+    plot(sparsity)
+    dev.off()
 
+}
+if(args$hist != "")
+{
+    png(args$hist)
+    hist(as.vector(as.matrix(dat)), main = "Distribution of values", breaks = 50, xlab = "Matrix entries")
+    dev.off()
 
+}
