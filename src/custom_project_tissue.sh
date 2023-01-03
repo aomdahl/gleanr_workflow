e312b5dcf0c9411cfbfcbe370a9fe47515da2b79
aomdahl <aomdahl1@gmail.com>
2023-01-03 15:12:35 -0500

    meta projection method

--- src/custom_project_tissue.sh
+++ src/custom_project_tissue.sh
@@ -8,23 +8,29 @@ mkdir -p $ID
 ml anaconda
 conda activate renv
 set -e
+
+if [[ "$3" == "NAMES" ]]; then
+	A=".names"
+else
+	A=""
+fi
+echo "full_hapmap3_snps.z$A.tsv"
+
 #Project
 echo "Starting with projection"
 echo "Rscript $SRC/projectSumStats.R --output $ID/projected_hapmap3_loadings.txt --sumstats $VARIANTS/full_hapmap3_snps.z.tsv --id_type RSID --factors $FACTORS"
-#Rscript $SRC/projectSumStats.R --output $ID/projected_hapmap3_loadings.txt --sumstats $VARIANTS/full_hapmap3_snps.z.tsv --id_type RSID --factors $FACTORS
+Rscript $SRC/projectSumStats.R --output $ID/projected_hapmap3_loadings.txt --sumstats $VARIANTS/full_hapmap3_snps.z$A.tsv --id_type RSID --factors $FACTORS --proj_method meta
 
 #Make the GWAS files
 echo "Making the gwas files..."
 mkdir -p $ID/loading_ss_files_F
-Rscript /scratch16/abattle4/ashton/snp_networks/custom_l1_factorization/src/buildSumStats.R --projected_loadings $ID/projected_hapmap3_loadings.txt --samp_file $VARIANTS/full_hapmap3_snps.n.tsv --hapmap_list $hapmap_list --output $ID/loading_ss_files_F/ --factors $FACTORS
+Rscript /scratch16/abattle4/ashton/snp_networks/custom_l1_factorization/src/buildSumStats.R --projected_loadings $ID/projected_hapmap3_loadings.txt --samp_file $VARIANTS/full_hapmap3_snps.n$A.tsv --hapmap_list $hapmap_list --output $ID/loading_ss_files_F/ --factors $FACTORS --samp_counts avg
 
 #LDSC projection
-#bash src/tissue_testing_only.sh $ID $FACTORS
+bash src/tissue_testing_only.sh $ID $FACTORS
 
 #rule ldsc_visualize:
-
-            #Rscript $SRC/visualizeLDSC.R --input_dir $ID/ --plot_type "fdr_sig" --output $ID/fdr_heatmap.png"  --extension "*.cell_type_results.txt
-            #Rscript $SRC/visualizeLDSC.R --input_dir $ID/ --plot_type "horizontal" --output $ID/full_heatmap.png"  --extension "*.cell_type_results.txt
+bash src/visualize_LDSC_custom.sh $ID/F_ldsc_enrichment_Multi_tissue_chromatin/ 
 
  
 
